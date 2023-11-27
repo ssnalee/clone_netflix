e70e8d15cbab77f7d5ad073d244fbab94317c391
ssnalee <97poolbbang@naver.com>
2023-11-27 18:08:51 +0900

    Updates

--- src/Routes/utils.ts
+++
@@ -1,9 +0,0 @@
-
-
-export function makeImagePath(id:string, format?:string){
-    if(id === null){
-        return "http://localhost:3000/logo.png";
-    } else{
-        return `https://image.tmdb.org/t/p/${format ? format : "original"}/${id}`;
-    }
-}
